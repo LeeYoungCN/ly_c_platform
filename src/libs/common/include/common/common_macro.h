4a341995823d6daa484e085024406af45200c898
LY_WSL <1175529741@qq.com>
2025-12-08 15:24:45 +0800

    common_macro

---
+++ src/libs/common/include/common/common_macro.h
@@ -0,0 +1,7 @@
+#ifndef COMMON_COMMON_MACRO_H
+#define COMMON_COMMON_MACRO_H
+
+#define COMM_MIN(a, b) (((a) > (b)) ? (b) : (a))
+#define COMM_MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+#endif // COMMON_COMMON_MACRO_H
